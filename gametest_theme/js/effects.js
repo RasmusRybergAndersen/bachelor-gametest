a336ee0123dcc88e0b440bb08e351b2a968ddaab
Warez92 <MortenN4444@msn.com>
2017-12-11 11:23:31 +0100

    .

--- gametest_theme/js/effects.js
+++ gametest_theme/js/effects.js
@@ -2,12 +2,12 @@ window.onload = function () {
     jQuery('.forside-slider').slick({
         draggable: true,
         arrows: false,
-        dots: false,
+        dots: true,
         autoplay: true,
         autoplaySpeed: 4000,
         fade: true,
         speed: 4000,
-        infinite: true,
+        infinite: true
 
     });
 }
